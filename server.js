87494d326bf34c486977c66fc8e69966949993bb
fazaimron27 <cincaumyself@gmail.com>
2022-07-12 22:37:30 +0700

    fix var port express

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ require("./app/routes/api/message.routes.js")(app);
 require("./app/routes/web/routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
